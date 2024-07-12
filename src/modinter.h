a6435ffc69639e84b328ba764296ed94ef755d52
dudu181 <3292572160@qq.com>
2024-07-12 10:01:10 +0800

    统一了.c和.h

--- src/modinter.h
+++ src/modinter.h
@@ -8,7 +8,7 @@ extern "C" {
 #include <time.h>
 #include "modfile.h"
 
-int AddTodoItem(TodoInfo *g_info, const char *name, const char *subtasks, const int *tags,
+int AddTodoItem(TodoInfo *g_info, const char *name, const char **subtasks,int subtaskCount, const int *tags,
        enum Priority priority, time_t startTime, time_t deadline, const char *desc);
 int DeleteTodoItem(TodoInfo *g_info, int itemIndex);
 int ModifyTodoItem(TodoInfo *g_info, int itemIndex, const char *name, const char *subtasks, 
